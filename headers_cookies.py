3fc7345fe5fdf0ce2cefcb11f53fde9327a1d623
Shirin Dehghani <shirin.dehghani1996@gmail.com>
2025-02-16 08:32:14 +0330

    remove comments & import headers, cookies

---
+++ headers_cookies.py
@@ -0,0 +1,27 @@
+cookies = {
+    'device_id': 'eyJpdiI6InpVenlvUzJnR3lVcEQ5RGdvamNhMGc9PSIsInZhbHVlIjoiOXhvSEtFYm85ZXJHYTdXaTRIenBKdz09IiwibWFjIjoiNzU2NGYzNjY2MDhjZjE1N2JhYzUwNTg5MjkwZTVmMjk1ZjYxYjFiYThiOTI4ZjQ0Mzg5NzQ2YjhkZWI2YjgxZiJ9',
+    'emp_pricing_test': 'eyJpdiI6IjBRWXdJYkhleUJaaUp2YUpLbkNCckE9PSIsInZhbHVlIjoiekZDU2N5eGl2c0JDWmQybzRMQ0hYdz09IiwibWFjIjoiODUzYmQwNzdkMWFmZGMwNGMwOTcxOTdlOGUwZGYyMThhOWFlMDRiN2EyYmRmOWViNzNmMmFlYmI5NWI1YzNkZCJ9',
+    'e_b_check': 'eyJpdiI6IlBycnBEc0JNTVZzMGtWZzNcL2R1Vmx3PT0iLCJ2YWx1ZSI6ImY3RVg2M1JLQUhBdWszMkdPRnhQQkE9PSIsIm1hYyI6IjQzMThjMGM2NjEzZGY4NTU0NjZlNDMyYjRkZDc0NDM1NGQzMGZiNTJhZTkxMDFmNmU3NmRiODYxMjNmZWYyNTUifQ%3D%3D',
+    'remember_82e5d2c56bdd0811318f0cf078b78bfc': 'eyJpdiI6IjN4Q3VlMlZYa2xXWDFoV1RPSFp2Y0E9PSIsInZhbHVlIjoiaE15cEVHXC9DejIydnBtMlJreENCcktncWRDUWFVc3pnRnZjOU1OQkZQa0d4UnBFc3FLc3lWOWNzZ2RtVnl4M0o5WERBa2pDN2hQa3RpN1FySmp5N2dHamtDR0VMQ3ZVUzJ0aVRSemRoRk1JPSIsIm1hYyI6Ijg5M2JhZjk1YTQ2YjQyNDhkMTg4MzIzMjk3YTFjNjMyYmZhNjVlMmU3YjVkZmZhNWI0MDhlZGRiNjMzZDE0OWEifQ%3D%3D',
+    'XSRF-TOKEN': 'eyJpdiI6ImRKeVVwNlVBMldodGEyWkxJY3VBRGc9PSIsInZhbHVlIjoiOEFWVVdmbStOWkhYRW5nK3ZMem1KUVhIOTNDWVVZYWdxcWs0Y094OHpOd1V3bGRIbWhuRFhjc3RST1RuN3RWVDREWVI1aERjcHpcL2h3SmVWdU10amlRPT0iLCJtYWMiOiIzNDI0M2E4ZTU3ZTIyYjcwZjdmNmVjNDUwMDBkNTljZDhmNWEyZTZiZTBlZWJmMDU3OGRlM2RkMTBkMWQyZGY0In0%3D',
+    'JSESSID': 'eyJpdiI6ImJheDNPQ0tBMVlBQnlmUEN2bFdFSVE9PSIsInZhbHVlIjoiMnlmNjgwa29tdlhadTFTTHI3bHgzN0NndjRuVnppYmxJZnZKdmlLQUlCRWNnRnE5Qzl5bjBuTGJJNndGcCtpRVZObXR4TEZEYXFWZTNESHVEVnJ0T3c9PSIsIm1hYyI6Ijg0MDgxZGY4ZmQ2YWMxZmZlNmE2MTc2ZjhlOTZkZmViOGQ5OTMxMWNiNGUyNmJiZGE1OGUzMDViNjFhOTJmM2UifQ%3D%3D',
+}
+
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9,fa;q=0.8',
+    'cache-control': 'max-age=0',
+    # 'cookie': '_ga=GA1.1.625526985.1734855928; analytics_token=eedda508-0df5-9cb6-881c-39e2894bf9a3; _yngt=897d8b10-9e4b-4ed9-63bc-52cde36160fe; device_id=eyJpdiI6InpVenlvUzJnR3lVcEQ5RGdvamNhMGc9PSIsInZhbHVlIjoiOXhvSEtFYm85ZXJHYTdXaTRIenBKdz09IiwibWFjIjoiNzU2NGYzNjY2MDhjZjE1N2JhYzUwNTg5MjkwZTVmMjk1ZjYxYjFiYThiOTI4ZjQ0Mzg5NzQ2YjhkZWI2YjgxZiJ9; _ga_T8HC2S1534=GS1.1.1736916532.9.1.1736917433.58.0.0; emp_pricing_test=eyJpdiI6IjBRWXdJYkhleUJaaUp2YUpLbkNCckE9PSIsInZhbHVlIjoiekZDU2N5eGl2c0JDWmQybzRMQ0hYdz09IiwibWFjIjoiODUzYmQwNzdkMWFmZGMwNGMwOTcxOTdlOGUwZGYyMThhOWFlMDRiN2EyYmRmOWViNzNmMmFlYmI5NWI1YzNkZCJ9; e_b_check=eyJpdiI6IlBycnBEc0JNTVZzMGtWZzNcL2R1Vmx3PT0iLCJ2YWx1ZSI6ImY3RVg2M1JLQUhBdWszMkdPRnhQQkE9PSIsIm1hYyI6IjQzMThjMGM2NjEzZGY4NTU0NjZlNDMyYjRkZDc0NDM1NGQzMGZiNTJhZTkxMDFmNmU3NmRiODYxMjNmZWYyNTUifQ%3D%3D; doc_notif=eyJpdiI6ImxDdTlianlmVEsxR0hVM0pyanR1b2c9PSIsInZhbHVlIjoiaVBkN0NLM1wvZTBGYzRTa0tSTkZxUHBpcVU3OTdGNEcxQUFFWVZoblVERlU9IiwibWFjIjoiZmY2OTczM2FiZWYxM2RjOWNhNDc2MDBjN2M4MTY4ZTI0NTVhZGFjMzZiYjk3Y2M4OGU4ODMwMTg3ZDY3MmE4NiJ9; remember_82e5d2c56bdd0811318f0cf078b78bfc=eyJpdiI6IjN4Q3VlMlZYa2xXWDFoV1RPSFp2Y0E9PSIsInZhbHVlIjoiaE15cEVHXC9DejIydnBtMlJreENCcktncWRDUWFVc3pnRnZjOU1OQkZQa0d4UnBFc3FLc3lWOWNzZ2RtVnl4M0o5WERBa2pDN2hQa3RpN1FySmp5N2dHamtDR0VMQ3ZVUzJ0aVRSemRoRk1JPSIsIm1hYyI6Ijg5M2JhZjk1YTQ2YjQyNDhkMTg4MzIzMjk3YTFjNjMyYmZhNjVlMmU3YjVkZmZhNWI0MDhlZGRiNjMzZDE0OWEifQ%3D%3D; analytics_campaign={%22source%22:%22localhost%22%2C%22medium%22:%22referral%22}; _yngt_iframe=1; logglytrackingsession=6bcfc8d1-19c2-4d94-bf71-19a5d4a098a4; user_mode=eyJpdiI6IlkyY2crMU00NzF6SzB4eEFDOGY0aEE9PSIsInZhbHVlIjoiZ0ltTjFCb0VDNWFoQWgzQXlVbk96UT09IiwibWFjIjoiYzA0OTFhMWZmZWRkM2ZkODNkMWNjOTFkODY3ZTJkMjVjZGJmMTBjNDc2YWMzOTM4MjgyOTk3YTkzYjc0ZDczOCJ9; analytics_session_token=90e314af-a538-b3d9-166f-fbccad006f03; yektanet_session_last_activity=2/16/2025; XSRF-TOKEN=eyJpdiI6ImRKeVVwNlVBMldodGEyWkxJY3VBRGc9PSIsInZhbHVlIjoiOEFWVVdmbStOWkhYRW5nK3ZMem1KUVhIOTNDWVVZYWdxcWs0Y094OHpOd1V3bGRIbWhuRFhjc3RST1RuN3RWVDREWVI1aERjcHpcL2h3SmVWdU10amlRPT0iLCJtYWMiOiIzNDI0M2E4ZTU3ZTIyYjcwZjdmNmVjNDUwMDBkNTljZDhmNWEyZTZiZTBlZWJmMDU3OGRlM2RkMTBkMWQyZGY0In0%3D; JSESSID=eyJpdiI6ImJheDNPQ0tBMVlBQnlmUEN2bFdFSVE9PSIsInZhbHVlIjoiMnlmNjgwa29tdlhadTFTTHI3bHgzN0NndjRuVnppYmxJZnZKdmlLQUlCRWNnRnE5Qzl5bjBuTGJJNndGcCtpRVZObXR4TEZEYXFWZTNESHVEVnJ0T3c9PSIsIm1hYyI6Ijg0MDgxZGY4ZmQ2YWMxZmZlNmE2MTc2ZjhlOTZkZmViOGQ5OTMxMWNiNGUyNmJiZGE1OGUzMDViNjFhOTJmM2UifQ%3D%3D',
+    'if-none-match': 'W/"6ec9850513f0e44bb9b7a47af7011b96"',
+    'priority': 'u=0, i',
+    'referer': 'https://jobinja.ir/jobs',
+    'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+}
